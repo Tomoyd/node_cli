66a92d37f698062eb41535984499bc173a0e8761
Tomoyd <ydh9419@163.com>
2021-02-03 22:56:37 +0800

    reset

--- first_cli/templates/javascript/src/index.js
+++
@@ -1,5 +0,0 @@
-import print from './printText';
-
-console.log('hello v5');
-
-print(124);
--- first_cli/templates/javascript/src/printText.js
+++
@@ -1,5 +0,0 @@
-function print(obj) {
-  const cText = obj.toString();
-  console.log({ cText });
-}
-export default print;
